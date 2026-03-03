30940385cac83542d25c1b10df3fb373741bd8fb
bOguzhan <me@oguzhanb.com>
2026-03-03 12:09:53 +0300

    feat: import fix on mdx file7

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,56 +1,55 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			pubDate: z.date(),
-			description: z.string(),
-			author: z.object({
-				name: z.string(),
-				link: z.string(),
-			}),
-			image: z.object({
-				source: image(),
-				alt: z.string(),
-			}),
-
-			tags: z.array(z.string()),
-		}),
+    schema: () =>
+        z.object({
+            title: z.string(),
+            pubDate: z.date(),
+            description: z.string(),
+            author: z.object({
+                name: z.string(),
+                link: z.string(),
+            }),
+            image: z.object({
+                source: z.string(), // Changed from image() to z.string()
+                alt: z.string(),
+            }),
+            tags: z.array(z.string()),
+        }),
 });
 
 const projectsCollection = defineCollection({
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			pubDate: z.date(),
-			description: z.string(),
-			link: z.string(),
-			author: z.object({
-				name: z.string(),
-				link: z.string(),
-			}),
-			image: z.object({
-				source: image(),
-				alt: z.string(),
-			}),
-		}),
+    schema: () =>
+        z.object({
+            title: z.string(),
+            pubDate: z.date(),
+            description: z.string(),
+            link: z.string(),
+            author: z.object({
+                name: z.string(),
+                link: z.string(),
+            }),
+            image: z.object({
+                source: z.string(), // Changed from image() to z.string()
+                alt: z.string(),
+            }),
+        }),
 });
 
 const authorsCollection = defineCollection({
-	schema: ({ image }) =>
-		z.object({
-			name: z.string(),
-			description: z.string(),
-			image: z.object({
-				source: image(),
-				alt: z.string(),
-			}),
-		}),
+    schema: () =>
+        z.object({
+            name: z.string(),
+            description: z.string(),
+            image: z.object({
+                source: z.string(), // Changed from image() to z.string()
+                alt: z.string(),
+            }),
+        }),
 });
 
 export const collections = {
-	posts: postsCollection,
-	projects: projectsCollection,
-	authors: authorsCollection,
+    posts: postsCollection,
+    projects: projectsCollection,
+    authors: authorsCollection,
 };
